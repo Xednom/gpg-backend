fd162001f4ae840ac6b69201129ce1a91f209896
Xednom <monde.lacanlalay@gmail.com>
2021-03-05 19:04:02 +0800

    routers for staff and client files

--- apps/gpg/urls.py
+++ apps/gpg/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from apps.authentication.views import StaffViewSet, ClientViewSet
+from apps.authentication.views import StaffViewSet, ClientViewSet, ClientFilesViewSet, StaffFilesViewSet
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r"staff", StaffViewSet, basename="staff-list")
 router.register(r"client", ClientViewSet, basename="client-list")
+router.register(r"client-files", ClientFilesViewSet, basename="client-files-list")
+router.register(r"staff-files", StaffFilesViewSet, basename="staff-files-list")
 router.register(r"job-order", views.JobOrderGeneralViewSet, basename="job-order-general")
 
 
