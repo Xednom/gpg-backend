18f656da038c6d33dac2b7b514e612145c95868e
Xednom <monde.lacanlalay@gmail.com>
2021-03-10 20:50:31 +0800

    added api for create comment in job order

--- apps/gpg/urls.py
+++ apps/gpg/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from apps.authentication.views import StaffViewSet, ClientViewSet, ClientFilesViewSet, StaffFilesViewSet
+from apps.authentication.views import (
+    StaffViewSet,
+    ClientViewSet,
+    ClientFilesViewSet,
+    StaffFilesViewSet,
+)
 from . import views
 
 router = routers.DefaultRouter()
@@ -9,10 +14,13 @@ router.register(r"staff", StaffViewSet, basename="staff-list")
 router.register(r"client", ClientViewSet, basename="client-list")
 router.register(r"client-files", ClientFilesViewSet, basename="client-files-list")
 router.register(r"staff-files", StaffFilesViewSet, basename="staff-files-list")
-router.register(r"job-order", views.JobOrderGeneralViewSet, basename="job-order-general")
+router.register(
+    r"job-order", views.JobOrderGeneralViewSet, basename="job-order-general"
+)
 
 
-app_name="gpg"
+app_name = "gpg"
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("job-order/<int:id>/comment/", views.CreateJobOrderComment.as_view(), name="job-order-comment")
 ]
