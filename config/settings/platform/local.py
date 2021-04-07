681296065ed94627fd5c941316dcaa7386d8cceb
Xednom <monde.lacanlalay@gmail.com>
2021-04-07 15:03:35 +0800

    changed database for local

--- config/settings/platform/local.py
+++ config/settings/platform/local.py
@@ -15,3 +15,18 @@ INTERNAL_IPS = [
 CRONJOBS = [
     ('* * * * *', 'django.core.management.call_command', ['send_mail']),
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'; \
+                            SET foreign_key_checks = 0",
+        },
+    }
+}
