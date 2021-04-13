e7414213e449b4f372f3c432a7e5f990b0d95e3f
Xednom <monde.lacanlalay@gmail.com>
2021-04-13 14:31:38 +0800

    removed dj-rest-auth urls

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -10,8 +10,4 @@ app_name = "auth"
 urlpatterns = [
     path("", include("djoser.urls")),
     path("", include("djoser.urls.jwt")),
-    path("dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
