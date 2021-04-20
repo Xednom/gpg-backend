e9bc0f6d77e20a4418c073dbeed192764aee52e9
Xednom <monde.lacanlalay@gmail.com>
2021-04-20 17:28:55 +0800

    removed pagination in state and county

--- apps/gpg/views/job_order_apn.py
+++ apps/gpg/views/job_order_apn.py
@@ -169,12 +169,14 @@ class CreateJobOrderByApnComment(generics.CreateAPIView):
 
 class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
+    pagination_class = None
     permission_classes = [permissions.IsAuthenticated]
     queryset = State.objects.all()
 
 
 class CountyViewSet(viewsets.ModelViewSet):
     serializer_class = CountySerializer
+    pagination_class = None
     permission_classes = [permissions.IsAuthenticated]
     queryset = County.objects.select_related("state").all()
     filter_backends = [filters.SearchFilter]
