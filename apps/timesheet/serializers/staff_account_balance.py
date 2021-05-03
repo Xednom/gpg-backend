803e333ad53da28fabe8261889f219d4aed8737a
Xednom <monde.lacanlalay@gmail.com>
2021-05-03 20:47:12 +0800

    added with currency in staff account balance

--- apps/timesheet/serializers/staff_account_balance.py
+++ apps/timesheet/serializers/staff_account_balance.py
@@ -9,7 +9,9 @@ __all__ = ("StaffAccountBalanceSerializer",)
 
 class StaffAccountBalanceSerializer(serializers.ModelSerializer):
     staff = serializers.PrimaryKeyRelatedField(queryset=Staff.objects.all(), required=False, allow_null=True)
+    payment_made_w_currency = serializers.CharField(source="payment_made")
     amount_due_w_currency = serializers.SerializerMethodField()
+    account_balance_w_currency = serializers.CharField(source="account_balance")
 
     class Meta:
         model = StaffAccountBalance
@@ -21,7 +23,9 @@ class StaffAccountBalanceSerializer(serializers.ModelSerializer):
             "amount_due_currency",
             "amount_due_w_currency",
             "payment_made",
+            "payment_made_w_currency",
             "account_balance",
+            "account_balance_w_currency",
             "notes"
         )
 
