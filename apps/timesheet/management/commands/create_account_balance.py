15cd13c2fa07f8207518eff7675cfe4e710c6953
Xednom <monde.lacanlalay@gmail.com>
2021-05-06 12:56:20 +0800

    added management command for timesheet auto-generate of account balances

---
+++ apps/timesheet/management/commands/create_account_balance.py
@@ -0,0 +1,56 @@
+from decimal import Decimal
+
+from django.db.models import Sum, Q
+from django.core.management.base import BaseCommand
+
+from post_office.models import EmailTemplate
+from post_office import mail
+
+from apps.authentication.models import Client
+from apps.timesheet.models import AccountCharge, PaymentHistory, AccountBalance
+
+
+class Command(BaseCommand):
+    help = "Automatically create Account balance for every user in the system monthly."
+
+    def handle(self, *args, **kwargs):
+        client_name = AccountCharge.objects.values_list("client", flat=True).distinct()
+        client = Client.objects.filter(id__in=client_name)
+        account_balance_deficit = AccountBalance.objects.filter(account_balance__lte=0.00)
+
+        for i in client:
+            client_balance = AccountBalance.objects.filter(client=i).exists()
+            client_time_charge = AccountCharge.objects.filter(client=i).aggregate(
+                totals=Sum("total_time"), total_charge=Sum("client_total_charge"),
+                total_due=Sum("client_total_due")
+            )
+            client_payment = PaymentHistory.objects.filter(client=i).aggregate(
+                total_payment=Sum("amount")
+            )
+            if client_balance:
+                AccountBalance.objects.filter(client=i).update(
+                    total_payment_made=client_payment["total_payment"],
+                    total_time_consumed=client_time_charge["totals"],
+                    account_charges=client_time_charge["total_due"],
+                    account_balance=client_payment["total_payment"] - client_time_charge["total_due"]
+                )
+                account_balance_deficit = AccountBalance.objects.filter(account_balance__lte=0.00)
+            else:
+                AccountBalance.objects.create(
+                    client=i,
+                    total_payment_made=client_payment["total_payment"],
+                    total_time_consumed=client_time_charge["totals"],
+                    account_charges=client_time_charge["total_due"],
+                    account_balance=client_payment["total_payment"] - client_time_charge["total_due"]
+                )
+        
+        for i in account_balance_deficit:
+
+            if account_balance_deficit:
+                mail.send(
+                    i.client.user.email,
+                    template="account_balance_deficit",
+                    context={
+                        "account_balance": i
+                    }
+                )
---
+++ apps/timesheet/management/commands/create_staff_account_balance.py
@@ -0,0 +1,88 @@
+import datetime
+
+from decimal import Decimal
+
+from django.db.models import Sum, Q
+from django.core.management.base import BaseCommand
+
+from apps.authentication.models import Staff
+from apps.timesheet.models import (
+    AccountCharge,
+    StaffPaymentHistory,
+    StaffAccountBalance,
+)
+
+
+class Command(BaseCommand):
+    help = "Automatically create Staff Account balance for every user in the system monthly(1st, 15th and 16th 30th-31st)."
+
+    def handle(self, *args, **kwargs):
+        staff_name = AccountCharge.objects.values_list("staff", flat=True).distinct()
+        staff = Staff.objects.filter(id__in=staff_name)
+        staff_timesheet = Staff.objects.filter(id__in=staff_name).exists()
+        current_time = datetime.date.today()
+        every_1st = datetime.date.today().replace(day=1)
+        every_15th = datetime.date.today().replace(day=15)
+        every_16th = datetime.date.today().replace(day=16)
+        every_31st = datetime.date.today().replace(day=31)
+        every_30th = datetime.date.today().replace(day=30)
+        day_1st_and_15th = str(every_1st) + " " + str(every_15th)
+        day_16th_and_30th = str(every_16th) + " " + str(every_30th)
+        day_16th_and_31st = str(every_16th) + " " + str(every_31st)
+
+        for i in staff:
+            if staff_timesheet:
+                staff_balance = StaffAccountBalance.objects.filter(staff=i).exists()
+                staff_time_charge_1st_and_15th = AccountCharge.objects.filter(
+                    staff=i, shift_date__gte=every_1st, shift_date__lte=every_15th
+                ).aggregate(
+                    totals=Sum("total_time"), total_charge=Sum("staff_total_due")
+                )
+                staff_time_charge_16th_and_30th = AccountCharge.objects.filter(
+                    staff=i, shift_date__gte=every_16th, shift_date__lte=every_30th
+                ).aggregate(
+                    totals=Sum("total_time"), total_charge=Sum("staff_total_due")
+                )
+                staff_time_charge_16th_and_31st = AccountCharge.objects.filter(
+                    staff=i, shift_date__gte=every_16th, shift_date__lte=every_31st
+                ).aggregate(
+                    totals=Sum("total_time"), total_charge=Sum("staff_total_due")
+                )
+                staff_payment_1st_and_15th = StaffPaymentHistory.objects.filter(
+                    staff=i, date__range=[every_1st, every_15th]
+                ).aggregate(total_payment=Sum("amount"))
+                staff_payment_16th_and_31st = StaffPaymentHistory.objects.filter(
+                    staff=i, date__gte=every_16th, date__lte=every_31st
+                ).aggregate(total_payment=Sum("amount"))
+                staff_payment_16th_and_30th = StaffPaymentHistory.objects.filter(
+                    staff=i, date__gte=every_16th, date__lte=every_30th
+                ).aggregate(total_payment=Sum("amount"))
+
+                if every_15th == current_time:
+                    print(staff_payment_1st_and_15th)
+                    StaffAccountBalance.objects.create(
+                        date=day_1st_and_15th,
+                        staff=i,
+                        payment_made=staff_payment_1st_and_15th["total_payment"],
+                        amount_due=staff_time_charge_1st_and_15th["total_charge"],
+                        account_balance=staff_payment_1st_and_15th["total_payment"]
+                        - staff_time_charge_1st_and_15th["total_charge"],
+                    )
+                elif every_31st == current_time:
+                    StaffAccountBalance.objects.create(
+                        date=day_16th_and_31st,
+                        staff=i,
+                        payment_made=staff_payment_16th_and_31st["total_payment"],
+                        amount_due=staff_time_charge_16th_and_31st["total_charge"],
+                        account_balance=staff_payment_16th_and_31st["total_payment"]
+                        - staff_time_charge_16th_and_31st["total_charge"],
+                    )
+                elif every_30th == current_time:
+                    StaffAccountBalance.objects.create(
+                        date=day_16th_and_30th,
+                        staff=i,
+                        payment_made=staff_payment_16th_and_30th["total_payment"],
+                        amount_due=staff_time_charge_16th_and_31st["total_charge"],
+                        account_balance=staff_payment_16th_and_30th["total_payment"]
+                        - staff_time_charge_16th_and_31st["total_charge"],
+                   )
