9bd7053d544174aeafe006ebf35a0198986f00ce
Xednom <monde.lacanlalay@gmail.com>
2021-05-11 15:56:05 +0800

    updated the management command for account balance

--- apps/timesheet/management/commands/create_account_balance.py
+++ apps/timesheet/management/commands/create_account_balance.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, Case, When, DecimalField
 from django.core.management.base import BaseCommand
 
 from post_office.models import EmailTemplate
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     help = "Automatically create Account balance for every user in the system monthly."
 
     def handle(self, *args, **kwargs):
-        client_name = AccountCharge.objects.values_list("client", flat=True).distinct()
+        client_name = AccountCharge.objects.filter(status="approved").values_list("client", flat=True).distinct()
         client = Client.objects.filter(id__in=client_name)
         account_balance_deficit = AccountBalance.objects.filter(
             account_balance__lte=0.00
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         for i in client:
             client_balance = AccountBalance.objects.filter(client=i).exists()
             client_time_charge = (
-                AccountCharge.objects.filter(client=i)
+                AccountCharge.objects.filter(client=i, status="approved")
                 .aggregate(
                     totals=Sum("total_time"),
                     total_charge=Sum("client_total_charge"),
@@ -33,26 +33,45 @@ class Command(BaseCommand):
             client_payment = PaymentHistory.objects.filter(client=i).aggregate(
                 total_payment=Sum("amount")
             )
-            if client_balance:
-                AccountBalance.objects.filter(client=i).update(
-                    total_payment_made=client_payment["total_payment"],
-                    total_time_consumed=client_time_charge["totals"],
-                    account_charges=client_time_charge["total_due"],
-                    account_balance=client_payment["total_payment"]
-                    - client_time_charge["total_due"],
-                )
-                account_balance_deficit = AccountBalance.objects.filter(
-                    account_balance__lte=0.00
-                )
+
+            if client_payment["total_payment"] == None:
+                if client_balance:
+                    AccountBalance.objects.filter(client=i).update(
+                        total_payment_made=Decimal(0.00),
+                        total_time_consumed=client_time_charge["totals"],
+                        account_charges=client_time_charge["total_due"],
+                        account_balance=Decimal(0.00)
+                        - client_time_charge["total_due"],
+                    )
+
+                else:
+                    AccountBalance.objects.create(
+                        client=i,
+                        total_payment_made=Decimal(0.00),
+                        total_time_consumed=client_time_charge["totals"],
+                        account_charges=client_time_charge["total_due"],
+                        account_balance=Decimal(0.00)
+                        - client_time_charge["total_due"],
+                    )
             else:
-                AccountBalance.objects.create(
-                    client=i,
-                    total_payment_made=client_payment["total_payment"],
-                    total_time_consumed=client_time_charge["totals"],
-                    account_charges=client_time_charge["total_due"],
-                    account_balance=client_payment["total_payment"]
-                    - client_time_charge["total_due"],
-                )
+                if client_balance:
+                    AccountBalance.objects.filter(client=i).update(
+                        total_payment_made=client_payment["total_payment"],
+                        total_time_consumed=client_time_charge["totals"],
+                        account_charges=client_time_charge["total_due"],
+                        account_balance=client_payment["total_payment"]
+                        - client_time_charge["total_due"],
+                    )
+
+                else:
+                    AccountBalance.objects.create(
+                        client=i,
+                        total_payment_made=client_payment["total_payment"],
+                        total_time_consumed=client_time_charge["totals"],
+                        account_charges=client_time_charge["total_due"],
+                        account_balance=client_payment["total_payment"]
+                        - client_time_charge["total_due"],
+                    )
 
         for i in account_balance_deficit:
 
