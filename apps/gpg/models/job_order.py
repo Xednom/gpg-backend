91277ec04e30d40d1daff0db46137af44e6b2aee
Xednom <monde.lacanlalay@gmail.com>
2021-05-17 18:30:26 +0800

    added email notif for creation of job order general and category

--- apps/gpg/models/job_order.py
+++ apps/gpg/models/job_order.py
@@ -1,3 +1,5 @@
+from post_office import mail
+
 from django.db import models
 from django.utils.functional import cached_property
 from django.contrib.auth import get_user_model
@@ -157,12 +159,28 @@ class JobOrderGeneral(TimeStamped):
         if not self.id:
             self.ticket_number = self.create_ticket_number()
             self.client_file = self.get_account_files()
+            mail.send(
+                "postmaster@landmaster.us",
+                bcc=["giliangegawin347@gmail.com", "admin@landmaster.us"],
+                template="job_order_create",
+                context={
+                    "job_order": self
+                }
+            )
             super(JobOrderGeneral, self).save(*args, **kwargs)
         elif self.id:
             self.ticket_number = self.create_ticket_number()
             self.client_file = self.get_account_files()
             self.client_email = self.get_client_email()
             self.staff_email = self.get_staff_email()
+            mail.send(
+                "postmaster@landmaster.us",
+                bcc=["giliangegawin347@gmail.com", "admin@landmaster.us"],
+                template="job_order_create",
+                context={
+                    "job_order": self
+                }
+            )
             super(JobOrderGeneral, self).save(*args, **kwargs)
 
 
