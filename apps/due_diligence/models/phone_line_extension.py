801629a84da1a7ec889439b31592225af021a93f
Xednom <monde.lacanlalay@gmail.com>
2021-05-17 13:09:39 +0800

    added allocation office for phone line extension

--- apps/due_diligence/models/phone_line_extension.py
+++ apps/due_diligence/models/phone_line_extension.py
@@ -7,6 +7,11 @@ from apps.authentication.models import Staff
 __all__ = ("PhoneLineExtension",)
 
 
+class AllocationOfficeChoices(models.TextChoices):
+    home_based = "home_based", ("Homebased")
+    office_based = "office_based", ("Office based")
+
+
 class PhoneLineExtension(TimeStamped):
     user_id = models.CharField(max_length=250)
     allocated_extension_staff = models.ManyToManyField(
@@ -15,6 +20,11 @@ class PhoneLineExtension(TimeStamped):
     original_extension_owner = models.CharField(max_length=250, blank=True)
     code_name = models.CharField(max_length=250)
     allocation_company = models.CharField(max_length=250)
+    allocation_office = models.CharField(
+        max_length=250,
+        choices=AllocationOfficeChoices.choices,
+        default=AllocationOfficeChoices.office_based,
+    )
 
     class Meta:
         ordering = ["-created_at"]
