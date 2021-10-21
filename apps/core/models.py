2dda6c97cd430012d1ccfe26f44128fcbee1240f
Xednom <monde.lacanlalay@gmail.com>
2021-10-21 22:32:30 +0800

    changed the value of the created_at in core

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TimeStamped(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
