47353bcfde5bf37f631b92b8203cf3a52a3dc814
Xednom <monde.lacanlalay@gmail.com>
2021-10-26 12:56:52 +0800

    updated the command for analytics

--- apps/gpg/management/commands/create_job_orders_analytics.py
+++ apps/gpg/management/commands/create_job_orders_analytics.py
@@ -45,39 +45,6 @@ class Command(BaseCommand):
             # job_count_jan = JobOrderCategory.objects.filter(
             #     client=i, created_at__month="01"
             # ).count()
-            # job_count_feb = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="02"
-            # ).count()
-            # job_count_mar = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="03"
-            # ).count()
-            # job_count_apr = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="04"
-            # ).count()
-            # job_count_may = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="05"
-            # ).count()
-            # job_count_jun = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="06"
-            # ).count()
-            # job_count_jul = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="07"
-            # ).count()
-            # job_count_aug = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="08"
-            # ).count()
-            # job_count_sep = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="09"
-            # ).count()
-            # job_count_oct = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="10"
-            # ).count()
-            # job_count_nov = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="11"
-            # ).count()
-            # job_count_dec = JobOrderCategory.objects.filter(
-            #     client=i, created_at__month="12"
-            # ).count()
             job_created = JobOrderCategoryAnalytics.objects.filter(
                 client=i, month_year=month_year
             ).exists()
@@ -99,73 +66,6 @@ class Command(BaseCommand):
                 #     job_count=job_count_jan,
                 #     month="Jan",
                 # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Feb/2021",
-                #     client=i,
-                #     job_count=job_count_feb,
-                #     month="Feb",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Mar/2021",
-                #     client=i,
-                #     job_count=job_count_mar,
-                #     month="Mar",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Apr/2021",
-                #     client=i,
-                #     job_count=job_count_apr,
-                #     month="Apr",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="May/2021",
-                #     client=i,
-                #     job_count=job_count_may,
-                #     month="May",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Jun/2021",
-                #     client=i,
-                #     job_count=job_count_jun,
-                #     month="Jun",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Jul/2021",
-                #     client=i,
-                #     job_count=job_count_jul,
-                #     month="Jul",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Aug/2021",
-                #     client=i,
-                #     job_count=job_count_aug,
-                #     month="Aug",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Sep/2021",
-                #     client=i,
-                #     job_count=job_count_sep,
-                #     month="Sep",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Oct/2021",
-                #     client=i,
-                #     job_count=job_count_oct,
-                #     month="Oct",
-                # )
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Nov/2021",
-                #     client=i,
-                #     job_count=job_count_nov,
-                #     month="Nov",
-                # )
-
-                # job_count = JobOrderCategoryAnalytics.objects.create(
-                #     month_year="Dec/2021",
-                #     client=i,
-                #     job_count=job_count_dec,
-                #     month="Dec",
-                # )
 
                 job_count = JobOrderCategoryAnalytics.objects.create(
                     month_year=month_year,
@@ -183,42 +83,9 @@ class Command(BaseCommand):
                 client=i, month_year=month_year
             ).exists()
 
-            job_count_jan = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="01"
-            ).count()
-            job_count_feb = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="02"
-            ).count()
-            job_count_mar = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="03"
-            ).count()
-            job_count_apr = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="04"
-            ).count()
-            job_count_may = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="05"
-            ).count()
-            job_count_jun = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="06"
-            ).count()
-            job_count_jul = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="07"
-            ).count()
-            job_count_aug = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="08"
-            ).count()
-            job_count_sep = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="09"
-            ).count()
-            job_count_oct = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="10"
-            ).count()
-            job_count_nov = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="11"
-            ).count()
-            job_count_dec = JobOrderGeneral.objects.filter(
-                client=i, created_at__month="12"
-            ).count()
+            # job_count_jan = JobOrderGeneral.objects.filter(
+            #     client=i, created_at__month="01"
+            # ).count()
 
             if job_order_general_created:
                 job_count = JobOrderGeneralAnalytics.objects.filter(
@@ -231,82 +98,15 @@ class Command(BaseCommand):
                 )
 
             else:
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Jan/2021",
-                    client=i,
-                    job_count=job_count_jan,
-                    month="Jan",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Feb/2021",
-                    client=i,
-                    job_count=job_count_feb,
-                    month="Feb",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Mar/2021",
-                    client=i,
-                    job_count=job_count_mar,
-                    month="Mar",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Apr/2021",
-                    client=i,
-                    job_count=job_count_apr,
-                    month="Apr",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="May/2021",
-                    client=i,
-                    job_count=job_count_may,
-                    month="May",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Jun/2021",
-                    client=i,
-                    job_count=job_count_jun,
-                    month="Jun",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Jul/2021",
-                    client=i,
-                    job_count=job_count_jul,
-                    month="Jul",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Aug/2021",
-                    client=i,
-                    job_count=job_count_aug,
-                    month="Aug",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Sep/2021",
-                    client=i,
-                    job_count=job_count_sep,
-                    month="Sep",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Oct/2021",
-                    client=i,
-                    job_count=job_count_oct,
-                    month="Oct",
-                )
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Nov/2021",
-                    client=i,
-                    job_count=job_count_nov,
-                    month="Nov",
-                )
-
-                job_count = JobOrderGeneralAnalytics.objects.create(
-                    month_year="Dec/2021",
-                    client=i,
-                    job_count=job_count_dec,
-                    month="Dec",
-                )
                 # job_count = JobOrderGeneralAnalytics.objects.create(
-                #     month_year=month_year,
+                #     month_year="Jan/2021",
                 #     client=i,
-                #     job_count=job_count,
-                #     month=str(calendar.month_abbr[today.month]),
+                #     job_count=job_count_jan,
+                #     month="Jan",
                 # )
+                job_count = JobOrderGeneralAnalytics.objects.create(
+                    month_year=month_year,
+                    client=i,
+                    job_count=job_count,
+                    month=str(calendar.month_abbr[today.month]),
+                )
