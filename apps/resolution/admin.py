a5bd8a8119ca186c37a78de34c84a7550f60f63f
Xednom <monde.lacanlalay@gmail.com>
2022-01-11 22:10:04 +0800

    added a resolution admin

--- apps/resolution/admin.py
+++ apps/resolution/admin.py
@@ -2,6 +2,53 @@ from django.contrib import admin
 
 from .models import Resolution, Category
 
+
+class ResolutionAdmin(admin.ModelAdmin):
+    model = Resolution
+    list_display = (
+        "date_submitted",
+        "description",
+        "assigned_to",
+        "client",
+        "resolution_provided_by_management",
+        "status",
+        "additional_notes",
+    )
+    list_filter = ("assigned_to", "client")
+    search_fields = (
+        "client__username",
+        "client__first_name",
+        "client__last_name",
+        "assigned_to__username",
+        "assigned_to__first_name",
+        "assigned_to__last_name",
+    )
+    fieldsets = (
+        (
+            "Resolution data",
+            {
+                "fields": (
+                    "date_submitted",
+                    "category",
+                    "status",
+                )
+            },
+        ),
+        (
+            "Info about the Resolution",
+            {
+                "fields": (
+                    "assigned_to",
+                    "client",
+                    "description",
+                    "resolution_provided_by_management",
+                    "additional_notes",
+                )
+            },
+        ),
+    )
+
+
 # Register your models here.
-admin.site.register(Resolution)
+admin.site.register(Resolution, ResolutionAdmin)
 admin.site.register(Category)
