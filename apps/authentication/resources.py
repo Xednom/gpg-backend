24e40fb1083dd23b17f25f03f38b7e6fe95df29f
Xednom <monde.lacanlalay@gmail.com>
2022-06-14 20:30:46 +0800

    #4 added export button

--- apps/authentication/resources.py
+++ apps/authentication/resources.py
@@ -1,5 +1,5 @@
 from import_export import resources
-from apps.authentication.models import Client
+from apps.authentication.models import Client, User
 
 
 class ClientResource(resources.ModelResource):
@@ -15,4 +15,18 @@ class ClientResource(resources.ModelResource):
             "lead_information",
             "customer_id",
             "hourly_rate",
+        )
+
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "designation_category",
+            "company_category",
         )
