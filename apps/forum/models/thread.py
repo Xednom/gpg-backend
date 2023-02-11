f2a92abcdbf7b4420a515ab80b0ec3d5ac8c6eee
Xednom <monde.lacanlalay@gmail.com>
2023-02-11 16:00:00 +0800

    #19 added status textchoice

--- apps/forum/models/thread.py
+++ apps/forum/models/thread.py
@@ -4,7 +4,7 @@ from django.db import models
 from apps.core.models import TimeStamped
 
 
-__all__ = ("Thread", "Comment", "Reply")
+__all__ = ("Thread", "Comment", "Reply", "Status")
 
 
 class Status(models.TextChoices):
